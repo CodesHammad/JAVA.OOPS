6b53e3f4e51305ac1c828188212c141fd7507c4f
Muhammad Hammad <mh0332392344@gmail.com>
2025-10-30 11:52:41 +0500

    update

--- Calendar/Calendar.java
+++ Calendar/Calendar.java
@@ -2,26 +2,37 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Scanner;
 
-public class Calender {
+public class Calendar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
-        int month = sc.nextInt();
+        System.out.print("Enter year: ");
 
+        int year = sc.nextInt();
+        System.out.print("Enter month: ");
+
+        int month = sc.nextInt();
         LocalDate date = LocalDate.of(year, month, 1);
-        DayOfWeek firstDay = date.getDayOfWeek();
+
         int daysInMonth = date.lengthOfMonth();
-        int start = firstDay.getValue(); 
-        if (start == 7) start = 0; 
+        DayOfWeek firstDay = date.getDayOfWeek();
 
         System.out.println("Calendar for the month of " + date.getMonth() + ", " + year);
-        System.out.println("Su  Mo  Tu  We  Th  Fr  Sa");
-        
-        for (int i = 0; i < start; i++) System.out.print("    ");
+        System.out.println("Su Mo Tu We Th Fr Sa");
+
+        int start = firstDay.getValue() % 7;
+
+        for (int i = 0; i < start; i++) {
+            System.out.print("   ");
+        }
 
         for (int day = 1; day <= daysInMonth; day++) {
-            System.out.printf("%-4d", day);
-            if ((day + start) % 7 == 0) System.out.println();
+            System.out.printf("%2d ", day);
+            if ((day + start) % 7 == 0) {
+                System.out.println();
+            }
         }
+
+        System.out.println();
+        sc.close();
     }
 }
